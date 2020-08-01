f55acdc1b2bfd50f0fc559cb9a374ba9ffb53963
sarabhadi <hadisarab007@gmail.com>
2020-08-01 11:34:40 -0500

    add HappyAgain.java

---
+++ src/HappyAgian.java
@@ -0,0 +1,8 @@
+public class HappyAgian {
+
+    public static void main(String[] args) {
+        int num = 10;
+        System.out.println(num);
+
+    }
+}
--- src/HelloB20.java
+++ src/HelloB20.java
@@ -2,5 +2,7 @@ public class HelloB20 {
 
     public static void main(String[] args) {
         System.out.println("Lily is funny");
+
+
     }
 }
