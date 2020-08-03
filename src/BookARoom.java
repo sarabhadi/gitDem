cbbe75c86c0c6f3183d8cf9da352a0fa7aa9386e
sarabhadi <67597059+sarabhadi@users.noreply.github.com>
2020-08-03 09:20:39 -0500

    Update BookARoom.java

--- src/BookARoom.java
+++ src/BookARoom.java
@@ -3,5 +3,7 @@ public class BookARoom {
     public static void main(String[] args) {
         String name = "sarab";
         System.out.println(name);
+        
+        // there is other way 
     }
 }
